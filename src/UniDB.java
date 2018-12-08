11ab9c49ad3dc058c1e7cef51a31e2ba03d32c73
jrumacias <juan@jrmacias.com>
2018-12-07 21:33:03 -0800

    add Calculate Average Grade by Section ID

--- src/UniDB.java
+++ src/UniDB.java
@@ -6,39 +6,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
 
-/**
- * This class demonstrates how to connect to MySQL and run some basic commands.
- * 
- * In order to use this, you have to download the Connector/J driver and add
- * its .jar file to your build path.  You can find it here:
- * 
- * http://dev.mysql.com/downloads/connector/j/
- * 
- * You will see the following exception if it's not in your class path:
- * 
- * java.sql.SQLException: No suitable driver found for jdbc:mysql://localhost:3306/
- * 
- * To add it to your class path:
- * 1. Right click on your project
- * 2. Go to Build Path -> Add External Archives...
- * 3. Select the file mysql-connector-java-5.1.24-bin.jar
- *    NOTE: If you have a different version of the .jar file, the name may be
- *    a little different.
- *    
- * The user name and password are both "root", which should be correct if you followed
- * the advice in the MySQL tutorial. If you want to use different credentials, you can
- * change them below. 
- * 
- * You will get the following exception if the credentials are wrong:
- * 
- * java.sql.SQLException: Access denied for user 'userName'@'localhost' (using password: YES)
- * 
- * You will instead get the following exception if MySQL isn't installed, isn't
- * running, or if your serverName or portNumber are wrong:
- * 
- * java.net.ConnectException: Connection refused
- */
-public class DBDemo {
+public class UniDB {
 
 	/** The name of the MySQL account to use (or empty for anonymous) */
 	private final String userName = "root";
@@ -117,6 +85,10 @@ public class DBDemo {
 			System.out.println("\n\n");
 			//Admin Request: Get lowest grades for all sections
 			viewLowestGradesBySection(conn);
+			System.out.println("\n\n");
+			// Admin Request: View average grade for specific course section
+			viewAvgGradeBySection(conn, 10006);
+			System.out.println("\n\n");
 
 		} catch (SQLException e) {
 //			System.out.println("ERROR: Could not connect to the database");
@@ -303,12 +275,47 @@ public class DBDemo {
 			e.printStackTrace();
 		}
 	}
+
+	public static void viewAvgGradeBySection(Connection conn, int sectionID) {
+		try {
+			String viewAvgGrades =
+					"SELECT sectionID, avg(grade) as avgGrade \r\n" +
+							"FROM Grade \r\n" +
+							"WHERE sectionID = ?;";
+			PreparedStatement pstmt = conn.prepareStatement(viewAvgGrades);
+			pstmt.setInt(1, sectionID);
+			ResultSet rs = pstmt.executeQuery();
+
+			System.out.println("Calculating average grade from section " + sectionID + "...");
+
+			System.out.println("-----------------------------");
+			System.out.format("%-14s %-4s ",
+					"Section ID", "Average Grade");
+			System.out.println();
+			System.out.println("-----------------------------");
+			while(rs.next()) {
+				String avgGrade = rs.getString("avgGrade");
+				String secID = rs.getString("sectionID");
+
+
+				System.out.format("%-14s %-4s ",
+						secID, avgGrade);
+				System.out.println();
+			}
+			System.out.println("-----------------------------");
+			System.out.println("Done.\n");
+			rs.close();
+		} catch(SQLException e) {
+			System.out.println("ERROR: Could not calculate average.");
+			e.printStackTrace();
+		}
+	}
 	
 	/**
 	 * Connect to the DB and do some stuff
 	 */
 	public static void main(String[] args) {
-		DBDemo app = new DBDemo();
+		UniDB app = new UniDB();
 		app.run();
 	}
 
